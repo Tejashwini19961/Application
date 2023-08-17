7f82b2de2e7b542efcd0152721522b0efe16f90d
Tejashwini R <89374814+Tejashwini19961@users.noreply.github.com>
2023-08-17 13:08:52 +0530

    Update program.java

--- program.java
+++ program.java
@@ -1,6 +1,3 @@
-// src/main/java/com/example/Main.java
-package com.example;
-
 public class Simple{
 public static void main(String args[]){
 System.out.println("Good Morning all\n\n This is training sessions provided by CloudyDoors Technology.\n\n I hope everything is going good.");
