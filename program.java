c4d16c6eec72957b7304ac2e45b11edc735a7ddf
Teju <tejashwiniraj1996@gmail.com>
2023-08-17 14:08:41 +0530

    first

--- program.java
+++ program.java
@@ -2,3 +2,4 @@ public class Simple{
 public static void main(String args[]){
 System.out.println("Good Morning all\n\n This is training sessions provided by CloudyDoors Technology.\n\n I hope everything is going good.");
 }}
+hi
